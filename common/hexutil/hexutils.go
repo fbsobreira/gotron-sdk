7770a0b2280584a9b13e4984131ba9b6fda3cfe5
sasaxie <xiexiaodong917@163.com>
2018-04-11 16:14:18 +0800

    utils: hex string to bytes: fix #1

--- common/hexutil/hexutils.go
+++ common/hexutil/hexutils.go
@@ -2,10 +2,31 @@ package hexutil
 
 import "encoding/hex"
 
+var (
+	EmptyString = &hexError{"empty hex string"}
+)
+
+type hexError struct {
+	msg string
+}
+
+func (h *hexError) Error() string {
+	return h.msg
+}
+
 // Encode encodes bytes as a hex string.
 func Encode(bytes []byte) string {
 	encode := make([]byte, len(bytes) * 2)
 	hex.Encode(encode, bytes)
 
 	return string(encode)
+}
+
+// Decode hex string as bytes
+func Decode(input string) ([]byte, error) {
+	if len(input) == 0 {
+		return nil, EmptyString
+	}
+
+	return hex.DecodeString(input[:])
 }
