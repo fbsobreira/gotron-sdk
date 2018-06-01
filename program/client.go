aa5fb1abb077d2e762e8a970aa351b1f37f11b2b
sasaxie <xiexiaodong917@163.com>
2018-06-01 12:47:27 +0800

    :recycle:add CMD:list witnesses

--- program/client.go
+++ program/client.go
@@ -11,35 +11,18 @@ import (
 )
 
 func main() {
-	address := flag.String("address", "",
+	grpcAddress := flag.String("grpcAddress", "",
 		"gRPC address: localhost:50051")
 
 	flag.Parse()
 
-	if strings.EqualFold("", *address) && len(*address) == 0 {
+	if strings.EqualFold("", *grpcAddress) && len(*grpcAddress) == 0 {
 		log.Fatalln("go run client -address localhost:50051")
 	}
 
-	client := service.NewGrpcClient(*address)
+	client := service.NewGrpcClient(*grpcAddress)
 	client.Start()
 	defer client.Conn.Close()
-
-	witnesses := client.ListWitnesses()
-
-	for i, v := range witnesses.Witnesses {
-		addr := hexutil.Encode(v.Address)
-		u := v.Url
-		totalProduced := v.TotalProduced
-		totalMissed := v.TotalMissed
-		latestBlockNum := v.LatestBlockNum
-		latestSlotNum := v.LatestSlotNum
-		isJobs := v.IsJobs
-		fmt.Printf("index: %d, witness: address: %s, url: %s, "+
-			"total produced: %d, total missed: %d, latest block num: %d, "+
-			"latest slot num: %d, is jobs: %v\n", i,
-			addr, u,
-			totalProduced, totalMissed, latestBlockNum, latestSlotNum, isJobs)
-	}
 
 	nodes := client.ListNodes()
 
---
+++ program/listwitnesses.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/tronprotocol/go-client-api/common/hexutil"
+	"github.com/tronprotocol/go-client-api/service"
+	"log"
+	"strings"
+)
+
+func main() {
+	grpcAddress := flag.String("grpcAddress", "",
+		"gRPC address: localhost:50051")
+
+	flag.Parse()
+
+	if strings.EqualFold("", *grpcAddress) && len(*grpcAddress) == 0 {
+		log.Fatalln("./list-witnesses -grpcAddress localhost:50051")
+	}
+
+	client := service.NewGrpcClient(*grpcAddress)
+	client.Start()
+	defer client.Conn.Close()
+
+	witnesses := client.ListWitnesses()
+
+	for i, v := range witnesses.Witnesses {
+		addr := hexutil.Encode(v.Address)
+		u := v.Url
+		totalProduced := v.TotalProduced
+		totalMissed := v.TotalMissed
+		latestBlockNum := v.LatestBlockNum
+		latestSlotNum := v.LatestSlotNum
+		isJobs := v.IsJobs
+		fmt.Printf("index: %d, witness: address: %s, url: %s, "+
+			"total produced: %d, total missed: %d, latest block num: %d, "+
+			"latest slot num: %d, is jobs: %v\n", i,
+			addr, u,
+			totalProduced, totalMissed, latestBlockNum, latestSlotNum, isJobs)
+	}
+}
