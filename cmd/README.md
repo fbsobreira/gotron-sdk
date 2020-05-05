922f3bbd16999d35af74dc4306d8d7f2b79336af
Fernando Sobreira <fbsobreira@gmail.com>
2020-05-05 18:43:07 -0300

    Add send/get adress

---
+++ cmd/README.md
@@ -0,0 +1,37 @@
+
+### CLI Actions
+- [ ] account
+  - [x] activate
+  - [x] balance
+  - [x] send
+  - [x] address
+  - [ ] freeze
+ 
+
+- [ ] trc10
+  - [x] issue
+  - [ ] byid
+  - [ ] byowner
+  - [ ] byname
+  - [ ] list
+  - [ ] send
+
+- [ ] trc20
+  - [ ] send
+  - [ ] balance
+  - [ ] allow
+  - [ ] sendfrom
+
+- [ ] contract
+  - [ ] constant
+  - [ ] send
+
+- [ ] bc
+  - [ ] mt
+  - [ ] block
+    - [ ] now
+    - [ ] byid
+    - [ ] byhash
+  - [ ] tx
+    - [ ] byid
+    - [ ] byhash
--- cmd/subcommands/account.go
+++ cmd/subcommands/account.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/fbsobreira/gotron-sdk/pkg/client/transaction"
 	"github.com/fbsobreira/gotron-sdk/pkg/common"
@@ -80,8 +82,13 @@ func accountSub() []*cobra.Command {
 			}
 
 			result := make(map[string]interface{})
+			result["txID"] = common.BytesToHexString(tx.GetTxid())
+			result["blockNumber"] = ctrlr.Receipt.BlockNumber
+			result["message"] = string(ctrlr.Result.Message)
 			result["receipt"] = map[string]interface{}{
-				"message": string(ctrlr.Receipt.Message),
+				"fee":      ctrlr.Receipt.Fee,
+				"netFee":   ctrlr.Receipt.Receipt.NetFee,
+				"netUsage": ctrlr.Receipt.Receipt.NetUsage,
 			}
 
 			asJSON, _ := json.Marshal(result)
@@ -90,7 +97,95 @@ func accountSub() []*cobra.Command {
 		},
 	}
 
-	return []*cobra.Command{cmdBalance, cmdActivate}
+	cmdSend := &cobra.Command{
+		Use:     "send <ADDRESS_TO> <AMOUNT>",
+		Short:   "send TRX to an address",
+		Args:    cobra.ExactArgs(2),
+		PreRunE: validateAddress,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if signerAddress.String() == "" {
+				return fmt.Errorf("no signer specified")
+			}
+			// get amount
+			value, err := strconv.ParseFloat(args[1], 64)
+			if err != nil {
+				return err
+			}
+			value = value * math.Pow10(6)
+			tx, err := conn.Transfer(signerAddress.String(), addr.String(), int64(value))
+			if err != nil {
+				return err
+			}
+
+			var ctrlr *transaction.Controller
+			if useLedgerWallet {
+				account := keystore.Account{Address: signerAddress.GetAddress()}
+				ctrlr = transaction.NewController(conn, nil, &account, tx.Transaction, opts)
+			} else {
+				ks, acct, err := store.UnlockedKeystore(signerAddress.String(), passphrase)
+				if err != nil {
+					return err
+				}
+				ctrlr = transaction.NewController(conn, ks, acct, tx.Transaction, opts)
+			}
+			if err = ctrlr.ExecuteTransaction(0); err != nil {
+				return err
+			}
+
+			if noPrettyOutput {
+				fmt.Println(tx)
+				return nil
+			}
+
+			result := make(map[string]interface{})
+			result["txID"] = common.BytesToHexString(tx.GetTxid())
+			result["blockNumber"] = ctrlr.Receipt.BlockNumber
+			result["message"] = string(ctrlr.Result.Message)
+			result["receipt"] = map[string]interface{}{
+				"fee":      ctrlr.Receipt.Fee,
+				"netFee":   ctrlr.Receipt.Receipt.NetFee,
+				"netUsage": ctrlr.Receipt.Receipt.NetUsage,
+			}
+
+			asJSON, _ := json.Marshal(result)
+			fmt.Println(common.JSONPrettyFormat(string(asJSON)))
+			return nil
+		},
+	}
+
+	cmdAddress := &cobra.Command{
+		Use:   "address [ACC_NAME]",
+		Short: "retrive address of account by name",
+		Args:  cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			address := ""
+			if len(args) == 0 {
+				if signerAddress.String() == "" {
+					return fmt.Errorf("no signer or account name specified")
+				}
+				address = signerAddress.String()
+			} else {
+				if err := validateAddress(cmd, args); err != nil {
+					return err
+				}
+				address = addr.String()
+			}
+
+			if noPrettyOutput {
+				fmt.Println(address)
+				return nil
+			}
+
+			result := make(map[string]interface{})
+			result["address"] = address
+
+			asJSON, _ := json.Marshal(result)
+			fmt.Println(common.JSONPrettyFormat(string(asJSON)))
+			return nil
+		},
+	}
+
+	return []*cobra.Command{cmdBalance, cmdActivate, cmdSend, cmdAddress}
 }
 
 func init() {
