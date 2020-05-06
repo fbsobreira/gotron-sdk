8b2daa4fb7138cc0f321a30b5e1a8cf0b17460b6
Fernando Sobreira <fbsobreira@gmail.com>
2020-05-06 13:31:28 -0300

    add deploy contract

--- pkg/client/contracts.go
+++ pkg/client/contracts.go
@@ -2,9 +2,14 @@ package client
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/fbsobreira/gotron-sdk/pkg/address"
 	"github.com/fbsobreira/gotron-sdk/pkg/proto/api"
 	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
+	"google.golang.org/protobuf/proto"
 )
 
 // TriggerConstantContract and return tx result
@@ -13,4 +18,67 @@ func (g *GrpcClient) TriggerConstantContract(ct *core.TriggerSmartContract) (*ap
 	defer cancel()
 
 	return g.Client.TriggerConstantContract(ctx, ct)
+}
+
+// DeployContract and return tx result
+func (g *GrpcClient) DeployContract(from, contractName string,
+	abi *core.SmartContract_ABI, codeStr string,
+	feeLimit, curPercent, oeLimit int64,
+) (*api.TransactionExtention, error) {
+
+	var err error
+
+	fromDesc, err := address.Base58ToAddress(from)
+	if err != nil {
+		return nil, err
+	}
+
+	if curPercent > 100 || curPercent < 0 {
+		return nil, fmt.Errorf("consume_user_resource_percent should be >= 0 and <= 100")
+	}
+	if oeLimit <= 0 {
+		return nil, fmt.Errorf("origin_energy_limit must > 0")
+	}
+
+	bc := common.FromHex(codeStr)
+
+	ct := &core.CreateSmartContract{
+		OwnerAddress: fromDesc.Bytes(),
+		NewContract: &core.SmartContract{
+			OriginAddress:              fromDesc.Bytes(),
+			Abi:                        abi,
+			Name:                       contractName,
+			ConsumeUserResourcePercent: curPercent,
+			OriginEnergyLimit:          oeLimit,
+			Bytecode:                   bc,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), grpcTimeout)
+	defer cancel()
+
+	tx, err := g.Client.DeployContract(ctx, ct)
+	if err != nil {
+		return nil, err
+	}
+	if feeLimit > 0 {
+		tx.Transaction.RawData.FeeLimit = feeLimit
+		// update hash
+		g.UpdateHash(tx)
+	}
+	return tx, err
+}
+
+// UpdateHash after local changes
+func (g *GrpcClient) UpdateHash(tx *api.TransactionExtention) error {
+	rawData, err := proto.Marshal(tx.Transaction.GetRawData())
+	if err != nil {
+		return err
+	}
+
+	h256h := sha256.New()
+	h256h.Write(rawData)
+	hash := h256h.Sum(nil)
+	tx.Txid = hash
+	return nil
 }
