2b12bc9f84e0bca27d099398d726847248dc1d88
Fernando Sobreira <fbsobreira@gmail.com>
2020-07-08 09:27:18 -0300

    Use ETH lib to parse Tron abi

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/fbsobreira/gotron-sdk
 go 1.14
 
 require (
+	github.com/OneOfOne/xxhash v1.2.5 // indirect
 	github.com/araddon/dateparse v0.0.0-20200409225146-d820a6159ab1
 	github.com/btcsuite/btcd v0.20.1-beta
 	github.com/cosmos/cosmos-sdk v0.38.3
 	github.com/deckarep/golang-set v1.7.1
-	github.com/ethereum/go-ethereum v1.9.13
+	github.com/elastic/gosigar v0.8.1-0.20180330100440-37f05ff46ffa // indirect
+	github.com/ethereum/go-ethereum v1.9.15
 	github.com/fatih/color v1.9.0
 	github.com/fatih/structs v1.1.0
 	github.com/gogo/protobuf v1.3.1
@@ -18,6 +20,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/rjeczalik/notify v0.9.2
 	github.com/shengdoushi/base58 v1.0.0
+	github.com/spaolacci/murmur3 v1.0.1-0.20190317074736-539464a789e9 // indirect
 	github.com/spf13/cobra v1.0.0
 	github.com/tyler-smith/go-bip39 v1.0.2
 	go.uber.org/zap v1.15.0
--- pkg/abi/abi.go
+++ pkg/abi/abi.go
@@ -3,8 +3,9 @@ package abi
 import (
 	"encoding/json"
 	"fmt"
-	"math/big"
 
+	eABI "github.com/ethereum/go-ethereum/accounts/abi"
+	eCommon "github.com/ethereum/go-ethereum/common"
 	"github.com/fbsobreira/gotron-sdk/pkg/address"
 	"golang.org/x/crypto/sha3"
 )
@@ -34,49 +35,73 @@ func Signature(method string) []byte {
 	return b[:4]
 }
 
+func convetToAddress(v interface{}) (eCommon.Address, error) {
+	switch v.(type) {
+	case string:
+		addr, err := address.Base58ToAddress(v.(string))
+		if err != nil {
+			return eCommon.Address{}, fmt.Errorf("invalid address %s: %+v", v.(string), err)
+		}
+		return eCommon.BytesToAddress(addr.Bytes()[len(addr.Bytes())-20:]), nil
+	}
+	return eCommon.Address{}, fmt.Errorf("invalid address %v", v)
+}
+
 // GetPaddedParam from struct
-func GetPaddedParam(p Param) ([]byte, error) {
-	for k := range p {
-		switch k {
-		case "uint16", "uint32", "uint64",
-			"int16", "int32", "int64":
-			n := big.NewInt(int64(p[k].(float64)))
-			b := make([]byte, 32-len(n.Bytes()))
-			return append(b, n.Bytes()...), nil
-		case "uint", "uint128", "uint256",
-			"int", "int128", "int256":
-			n, ok := new(big.Int).SetString(p[k].(string), 10)
-			if !ok {
-				return nil, fmt.Errorf("Invalid value %s", p[k].(string))
-			}
-			b := make([]byte, 32-len(n.Bytes()))
-			return append(b, n.Bytes()...), nil
-		case "address":
-			addr, err := address.Base58ToAddress(p[k].(string))
+func GetPaddedParam(param []Param) ([]byte, error) {
+	values := make([]interface{}, 0)
+	arguments := eABI.Arguments{}
+
+	for _, p := range param {
+		if len(p) != 1 {
+			return nil, fmt.Errorf("invalid param %+v", p)
+		}
+		for k, v := range p {
+			ty, err := eABI.NewType(k, "", nil)
 			if err != nil {
-				return nil, fmt.Errorf("Invalid value %s", p[k].(string))
+				return nil, fmt.Errorf("invalid parem %+v: %+v", p, err)
 			}
-			b := make([]byte, 12)
-			return append(b, addr[1:]...), nil
-		// TODO: decode another types
-		default:
-			return nil, fmt.Errorf("Invalid type %s", k)
+			arguments = append(arguments,
+				eABI.Argument{
+					Name:    "",
+					Type:    ty,
+					Indexed: false,
+				},
+			)
+
+			if ty.T == eABI.SliceTy || ty.T == eABI.ArrayTy {
+				if ty.Elem.T == eABI.AddressTy {
+
+					tmp := v.([]string)
+					v = make([]eCommon.Address, 0)
+					for i := range tmp {
+						addr, err := convetToAddress(tmp[i])
+						if err != nil {
+							return nil, err
+						}
+						v = append(v.([]eCommon.Address), addr)
+					}
+				}
+			}
+			if ty.T == eABI.AddressTy {
+				if v, err = convetToAddress(v); err != nil {
+					return nil, err
+				}
+			}
+			values = append(values, v)
 		}
 	}
-
-	return nil, nil
+	// convert params to bytes
+	return arguments.PackValues(values)
 }
 
 // Pack data into bytes
 func Pack(method string, param []Param) ([]byte, error) {
 	signature := Signature(method)
-	// convert params to bytes
-	for _, p := range param {
-		pBytes, err := GetPaddedParam(p)
-		if err != nil {
-			return nil, err
-		}
-		signature = append(signature, pBytes...)
+	pBytes, err := GetPaddedParam(param)
+	if err != nil {
+		return nil, err
 	}
+	signature = append(signature, pBytes...)
 	return signature, nil
 }
