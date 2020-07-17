3ab23205ca152f535598a46edf757d45bc428c19
Fernando Sobreira <fbsobreira@gmail.com>
2020-07-17 18:46:03 -0300

    Support json load abi int256 with string

--- pkg/abi/abi.go
+++ pkg/abi/abi.go
@@ -3,6 +3,9 @@ package abi
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
+	"reflect"
+	"strconv"
 
 	eABI "github.com/ethereum/go-ethereum/accounts/abi"
 	eCommon "github.com/ethereum/go-ethereum/common"
@@ -47,6 +50,37 @@ func convetToAddress(v interface{}) (eCommon.Address, error) {
 	return eCommon.Address{}, fmt.Errorf("invalid address %v", v)
 }
 
+func convertToInt(ty eABI.Type, v interface{}) interface{} {
+	if ty.T == eABI.IntTy && ty.Size <= 64 {
+		tmp, _ := strconv.ParseInt(v.(string), 10, ty.Size)
+		switch ty.Size {
+		case 8:
+			v = int8(tmp)
+		case 16:
+			v = int16(tmp)
+		case 32:
+			v = int32(tmp)
+		case 64:
+			v = int64(tmp)
+		}
+	} else if ty.T == eABI.UintTy && ty.Size <= 64 {
+		tmp, _ := strconv.ParseUint(v.(string), 10, ty.Size)
+		switch ty.Size {
+		case 8:
+			v = uint8(tmp)
+		case 16:
+			v = uint16(tmp)
+		case 32:
+			v = uint32(tmp)
+		case 64:
+			v = uint64(tmp)
+		}
+	} else {
+		v, _ = new(big.Int).SetString(v.(string), 10)
+	}
+	return v
+}
+
 // GetPaddedParam from struct
 func GetPaddedParam(param []Param) ([]byte, error) {
 	values := make([]interface{}, 0)
@@ -71,7 +105,6 @@ func GetPaddedParam(param []Param) ([]byte, error) {
 
 			if ty.T == eABI.SliceTy || ty.T == eABI.ArrayTy {
 				if ty.Elem.T == eABI.AddressTy {
-
 					tmp := v.([]string)
 					v = make([]eCommon.Address, 0)
 					for i := range tmp {
@@ -82,12 +115,27 @@ func GetPaddedParam(param []Param) ([]byte, error) {
 						v = append(v.([]eCommon.Address), addr)
 					}
 				}
+
+				if (ty.Elem.T == eABI.IntTy || ty.Elem.T == eABI.UintTy) &&
+					ty.Elem.Size > 64 &&
+					reflect.TypeOf(v).Elem().Kind() == reflect.String {
+					tmp := make([]*big.Int, 0)
+					for _, s := range v.([]string) {
+						value, _ := new(big.Int).SetString(s, 10)
+						tmp = append(tmp, value)
+					}
+					v = tmp
+				}
 			}
 			if ty.T == eABI.AddressTy {
 				if v, err = convetToAddress(v); err != nil {
 					return nil, err
 				}
 			}
+			if (ty.T == eABI.IntTy || ty.T == eABI.UintTy) && reflect.TypeOf(v).Kind() == reflect.String {
+				v = convertToInt(ty, v)
+			}
+
 			values = append(values, v)
 		}
 	}
--- pkg/abi/abi_test.go
+++ pkg/abi/abi_test.go
@@ -7,11 +7,24 @@ import (
 
 func TestABIParam(t *testing.T) {
 	//b, err := GetPaddedParam([]Param{{"address[2]": []string{"TEvHMZWyfjCAdDJEKYxYVL8rRpigddLC1R", "TEvHMZWyfjCAdDJEKYxYVL8rRpigddLC1R"}}})
+	ss, _ := new(big.Int).SetString("100000000000000000000", 10)
 	b, err := GetPaddedParam([]Param{
 		{"string": "KLV Test Token"},
 		{"string": "KLV"},
 		{"uint8": uint8(6)},
-		{"uint256": big.NewInt(1000000000)},
+		{"uint256": ss},
+	})
+	if err != nil {
+		t.Errorf(" %+v", err)
+	}
+	if len(b) != 256 {
+		t.Errorf("Wrong length %d/%d", len(b), 256)
+	}
+	b, err = GetPaddedParam([]Param{
+		{"string": "KLV Test Token"},
+		{"string": "KLV"},
+		{"uint8": "6"},
+		{"uint256": ss.String()},
 	})
 	if err != nil {
 		t.Errorf(" %+v", err)
@@ -30,4 +43,14 @@ func TestABIParamArray(t *testing.T) {
 		t.Errorf("Wrong length %d/%d", len(b), 64)
 	}
 
+}
+
+func TestABIParamArrayUint256(t *testing.T) {
+	b, err := GetPaddedParam([]Param{{"uint256[2]": []string{"100000000000000000000", "200000000000000000000"}}})
+	if err != nil {
+		t.Errorf(" %+v", err)
+	}
+	if len(b) != 64 {
+		t.Errorf("Wrong length %d/%d", len(b), 64)
+	}
 }
