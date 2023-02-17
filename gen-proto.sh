c5c130fe729fba0561e647c464140b4b2ca429ac
Fernando Sobreira <fbsobreira@gmail.com>
2023-02-17 05:46:55 -0500

    update tron protocol

--- gen-proto.sh
+++ gen-proto.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-protoc -I=./proto/tron -I/usr/lib -I./proto/googleapis --go_out=plugins=grpc,paths=source_relative:./pkg/proto ./proto/tron/core/*.proto ./proto/tron/core/contract/*.proto 
-protoc -I=./proto/tron -I/usr/lib -I./proto/googleapis --go_out=plugins=grpc,paths=source_relative:./pkg/proto ./proto/tron/api/*.proto
+
+INCLUDES="-I=./proto/tron -I/usr/lib -I./proto/googleapis"
+FLAGS="--go_out=./pkg/proto --go_opt paths=source_relative --go-grpc_out=./pkg/proto --go-grpc_opt=paths=source_relative"
+
+protoc ${INCLUDES} ${FLAGS} ./proto/tron/core/*.proto ./proto/tron/core/contract/*.proto ./proto/tron/api/*.proto
 mkdir -p ./pkg/proto/util
-protoc -I=./proto/tron -I./proto/util -I/usr/lib -I./proto/googleapis --go_out=plugins=grpc,paths=source_relative:./pkg/proto/util ./proto/util/*.proto
+protoc ${INCLUDES} -I=./proto/util --go_out=./pkg/proto/util --go_opt paths=source_relative ./proto/util/*.proto
