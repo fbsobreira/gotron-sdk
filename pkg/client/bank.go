4e8edbfe7103df8c5cf7f2ee11179cbb74033e1b
Gustavo Britto <gustavo@klever.io>
2023-03-14 09:18:21 -0300

    add staking 2.0 operations

--- pkg/client/bank.go
+++ pkg/client/bank.go
@@ -46,6 +46,35 @@ func (g *GrpcClient) FreezeBalance(from, delegateTo string,
 	return tx, nil
 }
 
+// FreezeBalance from base58 address
+func (g *GrpcClient) FreezeBalanceV2(from, delegateTo string,
+	resource core.ResourceCode, frozenBalance int64) (*api.TransactionExtention, error) {
+	var err error
+
+	contract := &core.FreezeBalanceV2Contract{}
+	if contract.OwnerAddress, err = common.DecodeCheck(from); err != nil {
+		return nil, err
+	}
+
+	contract.FrozenBalance = frozenBalance
+	contract.Resource = resource
+
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	tx, err := g.Client.FreezeBalanceV2(ctx, contract)
+	if err != nil {
+		return nil, err
+	}
+	if proto.Size(tx) == 0 {
+		return nil, fmt.Errorf("bad transaction")
+	}
+	if tx.GetResult().GetCode() != 0 {
+		return nil, fmt.Errorf("%s", tx.GetResult().GetMessage())
+	}
+	return tx, nil
+}
+
 // UnfreezeBalance from base58 address
 func (g *GrpcClient) UnfreezeBalance(from, delegateTo string, resource core.ResourceCode) (*api.TransactionExtention, error) {
 	var err error
@@ -75,6 +104,55 @@ func (g *GrpcClient) UnfreezeBalance(from, delegateTo string, resource core.Reso
 	}
 	if tx.GetResult().GetCode() != 0 {
 		return nil, fmt.Errorf("%s", tx.GetResult().GetMessage())
+	}
+	return tx, nil
+}
+
+// UnfreezeBalance from base58 address
+func (g *GrpcClient) UnfreezeBalanceV2(from string, unfreezeBalance int64, resource core.ResourceCode) (*api.TransactionExtention, error) {
+	var err error
+
+	contract := &core.UnfreezeBalanceV2Contract{}
+	if contract.OwnerAddress, err = common.DecodeCheck(from); err != nil {
+		return nil, err
+	}
+
+	contract.UnfreezeBalance = unfreezeBalance
+	contract.Resource = resource
+
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	tx, err := g.Client.UnfreezeBalanceV2(ctx, contract)
+	if err != nil {
+		return nil, err
+	}
+	if proto.Size(tx) == 0 {
+		return nil, fmt.Errorf("bad transaction")
+	}
+	if tx.GetResult().GetCode() != 0 {
+		return nil, fmt.Errorf("%s", tx.GetResult().GetMessage())
+	}
+	return tx, nil
+}
+
+func (g *GrpcClient) GetAvailableUnfreezeCount(from string) (*api.GetAvailableUnfreezeCountResponseMessage, error) {
+	var err error
+
+	contract := &api.GetAvailableUnfreezeCountRequestMessage{}
+	if contract.OwnerAddress, err = common.DecodeCheck(from); err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	tx, err := g.Client.GetAvailableUnfreezeCount(ctx, contract)
+	if err != nil {
+		return nil, err
+	}
+	if proto.Size(tx) == 0 {
+		return nil, fmt.Errorf("bad transaction")
 	}
 	return tx, nil
 }
--- pkg/client/resources.go
+++ pkg/client/resources.go
@@ -50,3 +50,117 @@ func (g *GrpcClient) GetDelegatedResources(address string) ([]*api.DelegatedReso
 	}
 	return result, nil
 }
+
+// GetDelegatedResourcesV2 from BASE58 address
+func (g *GrpcClient) GetDelegatedResourcesV2(address string) ([]*api.DelegatedResourceList, error) {
+	addrBytes, err := common.DecodeCheck(address)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	ai, err := g.Client.GetDelegatedResourceAccountIndexV2(ctx, GetMessageBytes(addrBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*api.DelegatedResourceList, len(ai.GetToAccounts()))
+	for i, addrTo := range ai.GetToAccounts() {
+		dm := &api.DelegatedResourceMessage{
+			FromAddress: addrBytes,
+			ToAddress:   addrTo,
+		}
+		resource, err := g.Client.GetDelegatedResourceV2(ctx, dm)
+		if err != nil {
+			return nil, err
+
+		}
+		result[i] = resource
+	}
+	return result, nil
+}
+
+// GetCanDelegatedMaxSize from BASE58 address
+func (g *GrpcClient) GetCanDelegatedMaxSize(address string, resource int32) (*api.CanDelegatedMaxSizeResponseMessage, error) {
+	addrBytes, err := common.DecodeCheck(address)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	dm := &api.CanDelegatedMaxSizeRequestMessage{}
+
+	dm.Type = resource
+	dm.OwnerAddress = addrBytes
+
+	response, err := g.Client.GetCanDelegatedMaxSize(ctx, dm)
+	if err != nil {
+		return nil, err
+
+	}
+
+	return response, nil
+}
+
+// DelegateResource from BASE58 address
+func (g *GrpcClient) DelegateResource(from, to string, resource core.ResourceCode, delegateBalance int64, lock bool) (*api.TransactionExtention, error) {
+	addrFromBytes, err := common.DecodeCheck(from)
+	if err != nil {
+		return nil, err
+	}
+
+	addrToBytes, err := common.DecodeCheck(to)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	contract := &core.DelegateResourceContract{}
+
+	contract.Resource = resource
+	contract.OwnerAddress = addrFromBytes
+	contract.ReceiverAddress = addrToBytes
+	contract.Lock = lock
+
+	response, err := g.Client.DelegateResource(ctx, contract)
+	if err != nil {
+		return nil, err
+
+	}
+
+	return response, nil
+}
+
+// UnDelegateResource from BASE58 address
+func (g *GrpcClient) UnDelegateResource(owner, receiver string, resource core.ResourceCode, delegateBalance int64, lock bool) (*api.TransactionExtention, error) {
+	addrOwnerBytes, err := common.DecodeCheck(owner)
+	if err != nil {
+		return nil, err
+	}
+
+	addrReceiverBytes, err := common.DecodeCheck(receiver)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := g.getContext()
+	defer cancel()
+
+	contract := &core.UnDelegateResourceContract{}
+
+	contract.Resource = resource
+	contract.OwnerAddress = addrOwnerBytes
+	contract.ReceiverAddress = addrReceiverBytes
+
+	response, err := g.Client.UnDelegateResource(ctx, contract)
+	if err != nil {
+		return nil, err
+
+	}
+
+	return response, nil
+}
