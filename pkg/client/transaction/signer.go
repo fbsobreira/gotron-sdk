237b471d113c9f04688fafd778ad13928f76a6f2
Fernando Sobreira <fbsobreira@gmail.com>
2025-07-23 18:07:43 -0400

    feat(transaction): implement SignTransaction function for transaction signing

--- pkg/client/transaction/signer.go
+++ pkg/client/transaction/signer.go
@@ -1,8 +1,36 @@
 package transaction
 
+import (
+	"crypto/sha256"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
+	proto "google.golang.org/protobuf/proto"
+)
+
 type SignerImpl int
 
 const (
 	Software SignerImpl = iota
 	Ledger
 )
+
+func SignTransaction(tx *core.Transaction, signer *btcec.PrivateKey) (*core.Transaction, error) {
+	rawData, err := proto.Marshal(tx.GetRawData())
+	if err != nil {
+		return nil, err
+	}
+
+	h256h := sha256.New()
+	h256h.Write(rawData)
+	hash := h256h.Sum(nil)
+
+	signature, err := crypto.Sign(hash, signer.ToECDSA())
+	if err != nil {
+		return nil, err
+	}
+	tx.Signature = append(tx.Signature, signature)
+
+	return tx, nil
+}
