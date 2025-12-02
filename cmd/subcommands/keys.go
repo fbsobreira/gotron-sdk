3ba53df486377ced44a124ecf1fb0ee907b94436
Fernando Sobreira <fbsobreira@gmail.com>
2025-12-02 15:30:54 -0500

    fix: replace deleted tyler-smith/go-bip39 with fbsobreira/go-bip39
      The upstream repository github.com/tyler-smith/go-bip39 has been deleted.
      This change migrates to github.com/fbsobreira/go-bip39 v1.2.0, a maintained fork created from a cached copy of the original library.
    
      Changes:
      - Replace go-bip39 import in cmd/subcommands/keys.go
      - Replace go-bip39 import in pkg/keys/mnemonic.go
      - Replace go-bip39 import in pkg/mnemonic/mnemonic.go
      - Update go.mod and go.sum with new dependency
      - Update golang.org/x dependencies to latest versions

--- cmd/subcommands/keys.go
+++ cmd/subcommands/keys.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/fatih/color"
+	"github.com/fbsobreira/go-bip39"
 	"github.com/fbsobreira/gotron-sdk/pkg/account"
 	"github.com/fbsobreira/gotron-sdk/pkg/address"
 	c "github.com/fbsobreira/gotron-sdk/pkg/common"
@@ -16,7 +17,6 @@ import (
 	"github.com/fbsobreira/gotron-sdk/pkg/mnemonic"
 	"github.com/fbsobreira/gotron-sdk/pkg/store"
 	"github.com/spf13/cobra"
-	"github.com/tyler-smith/go-bip39"
 	"golang.org/x/term"
 )
 
